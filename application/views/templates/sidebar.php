cee963e660419c4c2f29ae26baf7bbbcc747edb2
firmansyah120680 <firmansyahgro@gamil.com>
2022-04-26 12:46:51 +0700

    pustaka-booking

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -12,49 +12,97 @@
             <!-- Divider -->
             <hr class="sidebar-divider">
 
-           
+
 
             <!-- Looping Menu-->
-                <div class="sidebar-heading">
-                    Home
-                </div>
-                    <li class="nav-item active">
-                        <!-- Nav Item - Dashboard -->
-                        <li class="nav-item">
-                            <a class="nav-link pb-0" href="<?= base_url('admin'); ?>">
-                                <i class="fa fa-fw fa book"></i>
-                                <span>Dashboard</span></a>
-                        </li>
-                    </li>
+            <div class="sidebar-heading">
+                Home
+            </div>
+            <li class="nav-item active">
+                <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link pb-0" href="<?= base_url('admin'); ?>">
+                    <i class="fa fa-fw fa book"></i>
+                    <span>Dashboard</span></a>
+            </li>
+            </li>
 
-                <!-- Divider -->
-                <hr class="sidebar-divider mt-3">
+            <!-- Divider -->
+            <hr class="sidebar-divider mt-3">
 
-                <!-- Heading -->
-                <div class="sidebar-heading">
-                    Master Data
-                </div>
-                    <!-- Nav Item - Dashboard -->
-                    <li class="nav-item active">
-                        <!-- Nav Item - Dashboard -->
-                        <li class="nav-item">
-                            <a class="nav-link pb-0" href="<?= base_url('buku/kategori'); ?>">
-                                <i class="fa fa-fw fa book"></i>
-                                <span>Kategori Buku</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link pb-0" href="<?= base_url('buku'); ?>">
-                                <i class="fa fa-fw fa book"></i>
-                                <span>Data Buku</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link pb-0" href="<?= base_url('user/anggota'); ?>">
-                                <i class="fa fa-fw fa book"></i>
-                                <span>Data Anggota</span></a>
-                        </li>
-                    </li>
-                <!-- Divider -->
-                <hr class="sidebar-divider mt-3">
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Master Data
+            </div>
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link pb-0" href="<?= base_url('buku/kategori'); ?>">
+                    <i class="fa fa-fw fa book"></i>
+                    <span>Kategori Buku</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link pb-0" href="<?= base_url('buku'); ?>">
+                    <i class="fa fa-fw fa book"></i>
+                    <span>Data Buku</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link pb-0" href="<?= base_url('user/anggota'); ?>">
+                    <i class="fa fa-fw fa book"></i>
+                    <span>Data Anggota</span></a>
+            </li>
+            </li>
+            <!-- Divider -->
+            <hr class="sidebar-divider mt-3">
+
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Transaksi
+            </div>
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link pb-0" href="<?= base_url('pinjam'); ?>">
+                    <i class="fa fa-fw fa-shopping-cart"></i>
+                    <span>Data Peminjam</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link pb-0" href="<?= base_url('pinjam/daftarBooking'); ?>">
+                    <i class="fa fa-fw fa-list"></i>
+                    <span>Data Booking</span>
+            </li>
+            </li>
+            <!-- Divider -->
+            <hr class="sidebar-divider mt-3">
+
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Laporan
+            </div>
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link pb-0" href="<?= base_url('laporan/laporan_buku'); ?>">
+                    <i class="fa fa-fw fa-print"></i>
+                    <span>Laporan Data Buku</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link pb-0" href="<?= base_url('laporan/laporan_anggota'); ?>">
+                    <i class="fa fa-fw fa-book"></i>
+                    <span>Laporan Data Anggota</span>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link pb-0" href="<?= base_url('laporan/laporan_pinjam'); ?>">
+                    <i class="fa fa-fw fa-address-book"></i>
+                    <span>Laporan Peminjaman</span></a>
+            </li>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider mt-3">
 
             <!-- Sidebar Toggler (Sidebar) -->
             <div class="text-center d-none d-md-inline">
